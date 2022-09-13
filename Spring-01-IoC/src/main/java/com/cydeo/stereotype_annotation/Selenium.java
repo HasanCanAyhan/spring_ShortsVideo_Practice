5dd1afd9ef965db0855ade5926fad1a84fb9621a
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-09-13 15:00:32 +0200

    spring shorts practice

---
+++ Spring-01-IoC/src/main/java/com/cydeo/stereotype_annotation/Selenium.java
@@ -0,0 +1,12 @@
+package com.cydeo.stereotype_annotation;
+
+import org.springframework.stereotype.Component;
+
+
+public class Selenium {
+
+    public void getTeachingHours(){
+        System.out.println("Weekly teaching hours : " + 15);
+    }
+
+}
