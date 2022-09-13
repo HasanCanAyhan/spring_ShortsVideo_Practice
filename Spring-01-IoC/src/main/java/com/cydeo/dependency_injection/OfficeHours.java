ef99497dc7f9956dca4d37d99403cfb5751f0173
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-09-13 15:12:05 +0200

    spring shorts practice

---
+++ Spring-01-IoC/src/main/java/com/cydeo/dependency_injection/OfficeHours.java
@@ -0,0 +1,12 @@
+package com.cydeo.dependency_injection;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class OfficeHours {
+
+    public Integer getHours(){
+        return 5;
+    }
+
+}
