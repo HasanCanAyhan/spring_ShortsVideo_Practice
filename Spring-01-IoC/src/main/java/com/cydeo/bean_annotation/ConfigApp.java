c37990fd87fb82f575cbf3f658eef7ef0b108d2e
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-09-13 14:34:08 +0200

    spring shorts practice

--- Spring-01-IoC/src/main/java/com/cydeo/bean_annotation/ConfigApp.java
+++ Spring-01-IoC/src/main/java/com/cydeo/bean_annotation/ConfigApp.java
@@ -1,2 +1,20 @@
-package com.cydeo.bean_annotation;public class ConfigApp {
+package com.cydeo.bean_annotation;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigApp {
+
+    @Bean
+    FullTimeMentor fullTimeMentor(){
+        return new FullTimeMentor();
+    }
+
+    @Bean
+    PartTimeMentor partTimeMentor(){
+        return new PartTimeMentor();
+    }
+
+
 }
