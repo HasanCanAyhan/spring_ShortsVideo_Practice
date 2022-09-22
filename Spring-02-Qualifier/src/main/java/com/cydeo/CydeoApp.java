43418d21413e21e4ec9bfdc54547d5c324e9c99a
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-09-23 00:24:45 +0200

    spring shorts practice

--- Spring-02-Qualifier/src/main/java/com/cydeo/CydeoApp.java
+++ Spring-02-Qualifier/src/main/java/com/cydeo/CydeoApp.java
@@ -13,8 +13,31 @@ public class CydeoApp {
 
         ApplicationContext container = new AnnotationConfigApplicationContext(ConfigApp.class);
 
+
+        /*
+        JavaService java1 = container.getBean(JavaService.class);
+        JavaService java2 = container.getBean(JavaService.class);
+
+
+        // add @Scope("prototype) inside JavaService -->> that means creating different object
+
+        System.out.println(java1);
+        System.out.println(java2);
+        System.out.println(java1==java2);
+
+         */
+
+        // Eager _ vs_ Lazy
+        // In the Singleton Concept
+        // Eager is default , when we create container, all beans created
+        // with @Lazy annotation , it will be created only , whenever we call the class
+
+
         JavaService java = container.getBean(JavaService.class);
         java.getTeachingHours();
+
+
+
 
     }
 
--- Spring-02-Qualifier/src/main/java/com/cydeo/service/JavaService.java
+++ Spring-02-Qualifier/src/main/java/com/cydeo/service/JavaService.java
@@ -3,8 +3,13 @@ package com.cydeo.service;
 import com.cydeo.repository.JavaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+
+//@Scope("prototype")
+@Lazy
 @Component
 public class JavaService {
 
@@ -19,6 +24,7 @@ public class JavaService {
         this.officeHours = officeHours;
         this.mentoringHours = mentoringHours;
         this.javaRepository = javaRepository;
+        System.out.println("Eager Test");
     }
 
 
