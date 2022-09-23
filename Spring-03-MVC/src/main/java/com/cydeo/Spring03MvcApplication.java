70ac8a947537fcf3250b86829b198812fb34bbc4
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-09-23 13:52:47 +0200

    spring shorts practice

--- Spring-03-MVC/src/main/java/com/cydeo/Spring03MvcApplication.java
+++ Spring-03-MVC/src/main/java/com/cydeo/Spring03MvcApplication.java
@@ -1,9 +1,9 @@
-package com.cydeo.spring03mvc;
+package com.cydeo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // inside @Configuration and @ComponentScan
 public class Spring03MvcApplication {
 
 	public static void main(String[] args) {
