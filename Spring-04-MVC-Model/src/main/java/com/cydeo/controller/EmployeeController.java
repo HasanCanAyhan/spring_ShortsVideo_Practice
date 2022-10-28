0d9becb84bb85f14373fe13afc5e27e8d9059193
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-10-28 09:06:15 +0200

    spring shorts practice

--- Spring-04-MVC-Model/src/main/java/com/cydeo/controller/EmployeeController.java
+++ Spring-04-MVC-Model/src/main/java/com/cydeo/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.cydeo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/employee")
 public class EmployeeController {
 
-    //15-MVC-QUERY(REQUEST)PARAMETER
+    //Lesson 15-MVC-QUERY(REQUEST)PARAMETER
 
     @RequestMapping("/new") // /employee/new?firstName=Joe
     public String getInfo(@RequestParam(required = false,defaultValue = "TEST") String firstName, Model model){
@@ -18,6 +19,16 @@ public class EmployeeController {
         System.out.println(firstName);
         model.addAttribute("fName", firstName);
 
+        return "emp-register";
+    }
+
+    //Lesson 16-MVC-Path Parameter
+
+    @RequestMapping("/add/{firstName}/{lastName}") // /employee/add/firstName
+    public String getInfo2(@PathVariable String firstName,@PathVariable String lastName ,Model model){
+
+        model.addAttribute("fName", firstName);
+        model.addAttribute("lName", lastName);
         return "emp-register";
     }
 
