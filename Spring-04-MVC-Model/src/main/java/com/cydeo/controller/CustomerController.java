3a533b548bb7f536b973855841ea54f35b7f5383
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-10-30 05:55:09 +0100

    spring shorts practice

--- Spring-04-MVC-Model/src/main/java/com/cydeo/controller/CustomerController.java
+++ Spring-04-MVC-Model/src/main/java/com/cydeo/controller/CustomerController.java
@@ -5,11 +5,13 @@ import com.cydeo.enums.MembershipType;
 import com.cydeo.model.Customer;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 
@@ -29,7 +31,17 @@ public class CustomerController {
     }
 
     @PostMapping("/confirm")
-    public String confirm(@ModelAttribute("customer") Customer customer,Model model){
+    public String confirm(@Valid @ModelAttribute("customer") Customer customer, BindingResult bindingResult, Model model){
+
+        if (bindingResult.hasErrors()){//reloading
+
+            List<String> stateList = Arrays.asList("Alaska", "Alabama", "Arkansas", "American Samoa", "Arizona", "California", "Colorado", "Connecticut", "District of Columbia", "Delaware", "Florida", "Georgia", "Guam", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Virgin Islands", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming");
+
+            model.addAttribute("stateList", stateList);
+
+            return "customer/register";
+        }
+
 
         System.out.println(customer);
         model.addAttribute("customer", customer);
--- Spring-04-MVC-Model/src/main/java/com/cydeo/model/Customer.java
+++ Spring-04-MVC-Model/src/main/java/com/cydeo/model/Customer.java
@@ -5,21 +5,37 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Customer {
 
-
+    @NotBlank
+    @Size(min = 2, max = 20)
     private String firstName;
+
     private String lastName;
+
     private String address1;
+
     private String address2;
+
     private String state;
+
     private String zipCode;
+
     private String city;
+
+    @Email
     private String email;
+
     private MembershipType membershipType;
+
     private boolean agreeTerms;
 
 }
